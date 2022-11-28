96f10efff224eecf84b6fe7eb74054d16580ed04
Abdurashid-dev <abdurashiddev@gmail.com>
2022-11-28 15:04:05 +0500

    form added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-    <div class="container">
-
+    <div class="main">
+        <div class="title text-center p-3">
+            <h1>To Do List</h1>
+        </div>
+        <Form/>
     </div>
 </template>
 
 <script>
+import Form from "@/components/form/Form.vue";
 
+export default {
+    components: {Form}
+}
 </script>
 
 <style>
-
+.main {
+    max-width: 500px;
+    margin: 0 auto;
+}
 </style>
---
+++ src/components/form/Form.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <form class="d-flex justify-content-between">
+                <input type="text" name="content" class="form-control m-2">
+                <button type="submit" class="btn btn-primary m-2">Save!</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style>
+</style>
