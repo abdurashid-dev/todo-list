f9b0527e5287042a795790081ad662e5e8d581f8
Abdurashid-dev <abdurashiddev@gmail.com>
2022-11-28 15:38:57 +0500

    card added

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
             <h1>To Do List</h1>
         </div>
         <Form/>
+        <CardList/>
     </div>
 </template>
 
 <script>
 import Form from "@/components/form/Form.vue";
+import CardList from "@/components/cardList/CardList.vue";
 
 export default {
-    components: {Form}
+    components: {CardList, Form}
 }
 </script>
 
--- src/components/cardList/CardList.vue
+++ src/components/cardList/CardList.vue
@@ -1,9 +1,20 @@
 <template>
-
+    <div class="card mt-3">
+        <div class="card-header">
+            <div class="card-title text-center">
+                Todos
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="d-flex justify-content-between align-items-center">
+                <input type="checkbox" id="list">
+                <label for="list" class="user-select-none">Lorem ipsum dolor sit amet.</label>
+                <button class="btn btn-danger"><i class="fas fa-trash"></i></button>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
 
 <style></style>
